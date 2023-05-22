9aa98d999e9d7f6c1b063db6f5adb2b96eeafcdb
Tao888888 <43294584+Tao888888@users.noreply.github.com>
2023-05-22 16:15:31 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      name: 'Tao',
+      description: '欢迎访问<a href="http://exp.tao-space.top/" class="text-blue-500" target="_blank" >导航网站</a>',
     },
   }
 }
