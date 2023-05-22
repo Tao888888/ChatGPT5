593d221b9974a1d6c184ee8f73a3ab8d745210f0
Tao888888 <43294584+Tao888888@users.noreply.github.com>
2023-05-22 16:40:07 +0800

    Update index.vue

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -482,6 +482,19 @@ onUnmounted(() => {
             <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
               <span>与AI聊天</span>
             </div>
+						<div>
+						<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8272762342473783"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-8272762342473783"
+     data-ad-slot="7946032848"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+						</div>
           </template>
           <template v-else>
             <div>
