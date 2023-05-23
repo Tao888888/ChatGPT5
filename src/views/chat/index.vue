dabd152c3e6d5ddd6a76469a02188e5c21012229
Tao888888 <43294584+Tao888888@users.noreply.github.com>
2023-05-23 11:25:19 +0800

    Update index.vue

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -503,7 +503,7 @@ onUnmounted(() => {
                   <template #icon>
                     <SvgIcon icon="ri:stop-circle-line" />
                   </template>
-                  Stop Responding
+                  停止思考
                 </NButton>
               </div>
             </div>
